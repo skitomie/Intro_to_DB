2eb06c7de50c947c949511c6722e80010bdae2a5
skitomie <skitomie@gmail.com>
2024-07-28 04:36:41 +0100

    creating tables

---
+++ alx_book_store.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id FOREIGN KEY,
+    price FLOAT,
+    publication_date DATE
+);
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address VARCHAR(255)
+);
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id FOREIGN KEY,
+    order_date DATE
+);
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id FOREIGN KEY,
+    book_id FOREIGN KEY,
+    quantity FLOAT
+);

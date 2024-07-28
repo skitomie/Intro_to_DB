6354a66408b0160bf98cea6d0953d3d0bc0788cc
skitomie <skitomie@gmail.com>
2024-07-28 11:23:55 +0100

    more script

--- task_3.sql
+++ task_3.sql
@@ -5,8 +5,6 @@ import mysql.connector myDB = mysql.connector.connect(
     database = "alx_book_store"
 );
 mycursor = myDB.cursor();
-myTables = mycursor.execute(
-    ("USE DATABASE alx_book_store;");
-("SHOW TABLES;");
-);
+useDB = mycursor.execute("USE alx_book_store;");
+myTables = mycursor.execute("SHOW TABLES;");
 for tables in myTables: print(tables)

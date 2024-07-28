3cefedacbda19f3a698b3f5ee3fd0275e38f859c
skitomie <skitomie@gmail.com>
2024-07-28 13:33:54 +0100

    more script

--- task_5.sql
+++ task_5.sql
@@ -5,10 +5,9 @@ import mysql.connector myDB = mysql.connector.connect(
     database = "alx_book_store"
 );
 mycursor = myDB.cursor();
-sql = "INSERT INTO Customer(customerid, customer_name, email, address) VALUES (%s, %s, %s, %s)";
-val = (
-    1,
-    " Cole Baidoo ",
-    " cbaidoo @sandtech.com ",
-    " 123 Happiness Ave "
+sql = "(INSERT INTO customer (customer_id, customer_name, email, address) VALUES (%s,%s,%s,%s)" val = (
+    "1",
+    "Cole Baidoo",
+    "cbaidoo @sandtech.com ",
+    "123 Happiness Ave "
 ) mycursor.execute(sql, val) myDB.commit()

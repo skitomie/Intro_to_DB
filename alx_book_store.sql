08e9a72296affd2293623e8ecffb30c331bdd796
skitomie <skitomie@gmail.com>
2024-07-28 04:44:37 +0100

    creating tables

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -14,7 +14,7 @@ CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
-    address VARCHAR(255)
+    address TEXT
 );
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,

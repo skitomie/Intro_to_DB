5eddbf486510b90fdfd60edd39e9e9975ad8f2d8
skitomie <skitomie@gmail.com>
2024-07-28 12:17:59 +0100

    more script

--- MySQLServer.py
+++ MySQLServer.py
@@ -34,9 +34,15 @@ myDB = mysql.connector.connect(
     database = "alx_book_store"
 );
 mycursor = myDB.cursor();
-useDB = mycursor.execute
-(
-    "SELECT * 
-    FROM INFORMATION_SCHEMA.COLUMNS 
-    WHERE TABLE_NAME = books AND TABLE_SCHEMA = alx_book_store;"
-);
+useDB = mycursor.execute("SELECT
+        COLUMN_NAME, 
+        COLUMN_TYPE, 
+        IS_NULLABLE, 
+        COLUMN_DEFAULT, 
+        COLUMN_KEY, 
+        EXTRA 
+
+        FROM 
+        INFORMATION_SCHEMA.COLUMNS
+        WHERE 
+        TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store';")
